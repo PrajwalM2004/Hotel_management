0d4ab73323d1fd4ca502f951e5c07bf61c789235
Prajwal <01fe22bca003@kletech.ac.in>
2024-01-22 23:38:05 +0530

    Program to find fibnoci series

---
+++ fibnoci.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class fibnoci {
+    int fib(int n)
+        {
+            if(n==0)
+            {
+                return 0;
+            }
+            else if(n==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return fib(n-1) + fib(n-2);
+            }
+        }
+    public static void main(String args[])
+    {
+        
+        fibnoci f =new fibnoci();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n");
+        int n = sc.nextInt();
+        for(int i =0;i<n;i++)
+        {
+            System.out.println(f.fib(i));
+        }
+    } 
+}
