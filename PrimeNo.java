be03f833507660b89ed28d9b4db2800d408d8c78
Prajwal <01fe22bca003@kletech.ac.in>
2024-01-24 13:12:45 +0530

    prime number

---
+++ PrimeNo.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PrimeNo {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter starting range");
+        int a = s.nextInt();
+        System.out.println("Enter end range");
+        int b = s.nextInt();
+        
+        for (int i = a; i <= b; i++) {
+            boolean isPrime = true;
+
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            if (isPrime && i > 1) {
+                System.out.println(i);
+            }
+        }
+    }
+}
